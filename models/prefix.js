5862de97c4157f56070e6dda38b9578d1ee440de
hash-codes <57293314+hash-codes@users.noreply.github.com>
2019-11-02 16:07:45 +0100

    Delete prefix.js

--- models/prefix.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const prefixSchema = mongoose.Schema({
-  serverID: String,
-  prefix: String
-});
-
-module.exports = mongoose.model("Prefix", prefixSchema);
